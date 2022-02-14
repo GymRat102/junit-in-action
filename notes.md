164bb0d1619891f1964601fb3908c050cd73ea9f
GymRat102 <weijianduan0302@gmail.com>
2022-02-14 23:46:20 +0800

    [chapter1] Improved: simple calculator and java way of testing

--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 ### chaptor 1
 - one aspect of the point of automate test ---> test saves programmer of the action of typing the input
 - Java way of testing still need programmer to monitor the console output
+- seperate test case, make main method easier to maintain
+- skeleton code benefits you to run test and manage result
--- src/CaculatorTest.java
+++ src/CaculatorTest.java
@@ -1,13 +1,29 @@
 public class CaculatorTest {
-   public static void main(String[] args) {
+
+   private int nbErrors = 0;
+
+   // move the test into seperate method
+   // main method is easier to maintain
+   public void testAdd() {
       Calculator calculator = new Calculator();
-
-      // test saves programmer of the action of typing the input
       double result = calculator.add(10, 50);
-
-      // but programmer still needs to monitor the output
       if (result != 60) {
-         System.out.println("Bad result: " + result);
+         throw new IllegalStateException("Bad result: " + result);
+      }
+   }
+
+   public static void main(String[] args) {
+      CaculatorTest test = new CaculatorTest();
+
+      try {
+         test.testAdd();
+      } catch (Throwable e) {
+         test.nbErrors++;
+         e.printStackTrace();
+      }
+
+      if (test.nbErrors > 0) {
+         throw new IllegalStateException("There were " + test.nbErrors + "error(s)");
       }
    }
 }
