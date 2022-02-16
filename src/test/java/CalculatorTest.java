e35291c1f61fefc3d8577c3229521acebb4a5439
GymRat102 <weijianduan0302@gmail.com>
2022-02-16 21:12:11 +0800

    [chapter1] Junit5 version of the same test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(10, 50);
+        assertEquals(60, result, 0);
+    }
+}
